25a04cb758c0f60b56a3bec64490f94fc602af37
amar <amarpatait2001@gmail.com>
2025-08-12 18:21:14 +0530

    ms

--- src/Arrays/level1.java
+++ src/Arrays/level1.java
@@ -12,8 +12,53 @@ public class level1 {
 
         return large;
     }
+
+    public static int smallest(int[]arr){
+        int small = Integer.MAX_VALUE;
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i]<small){
+                small = arr[i];
+            }
+        }
+        return small;
+    }
+
+    public static int count(int[]arr){
+        int count = 0;
+        for (int i = 0; i<arr.length; i++){
+            count+=arr[i];
+        }
+        return count;
+    }
+
+    public static void countOddEven(int[]arr){
+        int odd = 0, Even = 0;
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i]%2==0){
+                Even++;
+            }else{
+                odd++;
+            }
+        }
+        System.out.println("Odd :"+odd);
+        System.out.println("Even :"+Even);
+    }
+
+    public static int[] reverseArray(int[]arr){
+        int[]reversed  = new int[arr.length];
+        int index = 0;
+        for (int i = arr.length-1; i>=0; i--){
+            reversed[index++] = arr[i];
+        }
+    }
     public static void main(String[] args) {
         int[]arr = {2,3,1,6,4};
         System.out.println(largest(arr));
+        System.out.println(smallest(arr));
+        System.out.println(count(arr));
+        countOddEven(arr);
+
     }
 }
